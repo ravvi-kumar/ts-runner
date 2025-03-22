ce065bb786bae27c2ad1ed9587bf06404bd3ba4d
ravvi-kumar <ravvi.kumar8989@gmail.com>
2025-03-22 15:39:27 +0530

    fix: build errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  worker: {
-    format: 'es',
-    plugins: () => []
-  },
-  optimizeDeps: {
-    include: ['typescript']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/typescript/]
-    }
-  }
 })
